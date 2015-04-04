5026341449cdeaa3a91b35e38377a3a9e54f55e6
Amit <mbuser@bigrock.local>
2015-04-03 20:00:49 -0700

    build basic VMs updates

--- tmpfileStore/devstackTmpFiles/build-ping-VMs.sh
+++ tmpfileStore/devstackTmpFiles/build-ping-VMs.sh
@@ -31,10 +31,11 @@ ssh-keygen -t rsa -f $sshKeyName -N ""
 neutron subnet-update  --dns-nameserver 8.8.8.8 private-subnet --dns-nameserver 8.8.4.4
 
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-neutron security-group-rule-create --protocol icmp --direction ingress default
-neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress default
+#neutron security-group-rule-create --protocol icmp --direction ingress default
+#neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress default
 
-
+cd
+cd ~/devstack
 nova boot --image trustyUbun --flavor m1.tmpbox --key-name $sshKeyName  worker10
 
 
